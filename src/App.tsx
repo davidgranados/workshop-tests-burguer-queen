02b217e4bf0096ff6394f1768e0b7eab27a2fed3
David <davidgranados73@gmail.com>
2023-09-11 07:50:40 -0500

    feat: agregar archivo css global

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "./router";
 
+import './App.css'
+
 export const App: FC = () => {
   return <RouterProvider router={router} />;
 };
