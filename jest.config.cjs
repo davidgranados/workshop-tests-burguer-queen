4984a4667b83e294eb01ec65a0e4792e883bb13c
David <davidgranados73@gmail.com>
2023-09-11 09:13:45 -0500

    tests: jest setup paso 2 css

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  roots: ["<rootDir>/src"],
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
@@ -6,4 +7,7 @@ module.exports = {
   transformIgnorePatterns: [
     "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
   ],
+  moduleNameMapper: {
+    "^.+\\.css$": "<rootDir>/src/__mocks__/file-mock.cjs",
+  },
 };
