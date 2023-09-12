99232b5a5369ec75271d02991bbb33b5b25cdfdb
David <davidgranados73@gmail.com>
2023-09-12 08:30:11 -0500

    feat: agregar imagen al home

--- src/__tests__/example.test.tsx
+++ src/__tests__/example.test.tsx
@@ -1,9 +1,6 @@
 import { render, screen } from "@testing-library/react";
 
 import { Home } from "../pages/home";
-import { App } from "../App";
-
-jest.mock('../services/auth-service.ts')
 
 describe("App", () => {
   it("should should be a teapot", () => {
@@ -12,11 +9,6 @@ describe("App", () => {
 
   it("should render Home", () => {
     render(<Home />);
-    screen.debug();
-  });
-
-  it("should render App", () => {
-    render(<App />);
     screen.debug();
   });
 });
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+import viteLogo from "../../assets/vite-logo.png";
+
+export const Home: FC = () => {
+  return (
+    <div>
+      Home
+      <img src={viteLogo} alt="Vite Logo" />
+    </div>
+  );
+};
