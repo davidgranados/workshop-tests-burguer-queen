21ea21db6ed3510b177235d975c63e250c90a259
David <davidgranados73@gmail.com>
2023-09-12 08:09:53 -0500

    update example tests

--- src/__tests__/example.test.tsx
+++ src/__tests__/example.test.tsx
@@ -1,22 +1,14 @@
 import { render, screen } from "@testing-library/react";
 
 import { Home } from "../pages/home";
-import { App } from "../App";
 
-jest.mock('../services/auth-service.ts')
-
-describe("App", () => {
+describe("Examples", () => {
   it("should should be a teapot", () => {
     expect(1).toBe(1);
   });
 
   it("should render Home", () => {
     render(<Home />);
-    screen.debug();
-  });
-
-  it("should render App", () => {
-    render(<App />);
     screen.debug();
   });
 });
